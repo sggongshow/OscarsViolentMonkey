27387d33c71eb371b50277f9f67cc82d4308fd4f
sggongshow <87620031+sggongshow@users.noreply.github.com>
2022-06-06 21:48:43 -0700

    Update CorticoMod.user.js

--- CorticoMod.user.js
+++ CorticoMod.user.js
@@ -5,38 +5,64 @@
 // @include     */casemgmt/forward.jsp?action=view&demographic*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version			22.05.17.5
+// @version			22.06.06.0
 // ==/UserScript==
 
+var checkCort  // make the Cortico Box available globally
+
+var observer = new MutationObserver(function (event) { 
+  
+  var boxCort = checkCort[0] //Get the actuall Element for the Cortico Icon
+  var boxCortClass = boxCort.className //get the class information for Cortico Icon
+  
+  if (boxCortClass.includes("tw-left-5")==false){ //check if it has already been corrected
+    setTimeout(function(){ changeLocation(); }, 1000)
+  }
+})
+
 function main(){
-	console.log("CHECK CORTICO Relocation")
-  var checkCort = $('[class*=tw-bg-indigo-60][class*=tw-transition-colors][class*=hover]')
-  //console.log(checkCort)
-  if (checkCort.length>0){
-    //console.log(checkCort)
-    //console.log("checkCort")
-   	main2(checkCort) 
+  //Get Cortico Icon Element
+  checkCort = $('[class*=cortico-widget-body][class*=tw-fixed][class*=tw-text-white]') 
+  
+  console.log(checkCort)
+  if (checkCort.length>0){ 
+    main3()
   }
   
 }
 
-function main2(checkCort){
+function changeLocation(){
 
-  var boxCort = checkCort[0].parentElement
-  var boxCortClass = boxCort.className
-  var boxCortArr = boxCortClass.split('tw-right')
+  //Get Cortico Icon Array
+  checkCort = $('[class*=cortico-widget-body][class*=tw-fixed][class*=tw-text-white]')
+  var boxCort = checkCort[0] //Get the actual Cortico Element
+  var boxCortClass = boxCort.className //Get th Cortico Element class name
+  //Change from right align to left aligh
+  var boxCortArr = boxCortClass.split('tw-right') 
   var newClass = boxCortArr[0] + 'tw-left' + boxCortArr[1]
+  //Set class to new information and style to custom style 
   boxCort.setAttribute('class',newClass)
-  boxCort["style"]= "z-index:1;position:fixed;bottom:60px;left:20px;"
+  boxCort["style"]= "z-index:1;position:fixed;bottom:60px;left:20px"
 
 }
 
-
+function main3(){
+  
+  changeLocation()
+  
+  // Observe Mutation of Cortico Icon Element focused on class
+  observer.observe(checkCort[0], {
+    attributes: true, 
+    attributeFilter: ['class'],
+    childList: false, 
+    characterData: false
+  })
+    
+}
 
 
 window.addEventListener('load', function() {
   setTimeout(function(){ main(); }, 5000)
   //waitForNote()
-
 
 }, false);
