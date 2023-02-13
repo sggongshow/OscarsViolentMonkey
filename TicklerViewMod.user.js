9d8b2875d323bbf0573505c1e0531deaa74ce095
sggongshow <sunny_g__happy@hotmail.com>
2023-02-12 23:11:11 -0800

    Revert "Update TicklerViewMod.user.js"
    This reverts commit ec7d89bbfbb1fa732cd283ff1542b7fd0d516682.

--- TicklerViewMod.user.js
+++ TicklerViewMod.user.js
@@ -5,11 +5,8 @@
 // @include     */ticklerDemoMain.jsp?*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version			23.02.12.0
+// @version			22.05.17.2
 // ==/UserScript==
-
-//changelog
-//23.02.12.0: changed to quipo: Create Report button jquery changed
 
 
 //wait window load first
@@ -17,7 +14,7 @@
 var myIDNum = '190'
 
 window.addEventListener('load', function() {
-
+  
   var compBut = document.createElement('input');
   compBut.type = 'button';
   compBut.id = 'compBut'
@@ -26,10 +23,10 @@ window.addEventListener('load', function() {
   compBut.onclick = compButFunc
   compBut.setAttribute('style', 'width:80px;font-size:12px;padding:0px;position:fixed;top:10px;right:10; background-color:cyan;');
 	document.body.appendChild(compBut);
-
+  
   //--------- select the textbox area so I can start typing immediately
   var textBox = $('textarea[name="textarea"]')
-  textBox.select()
+  textBox.select()  
 }, false);
 
 
@@ -40,16 +37,16 @@ document.addEventListener('keydown', function(theEvent) {
 	var theAltKey =theEvent.altKey;
 	var theCtrlKey = theEvent.ctrlKey;
 	var theShiftKey= theEvent.shiftKey;
-
-
+  
+  
   switch(true){
       //Acknowledge  button
-    case theAltKey && theKey==='1':
+    case theAltKey && theKey==='1': 
       compButFunc()
 			break;
 
     default:
-      break;
+      break; 
   }
 
 
@@ -59,8 +56,8 @@ document.addEventListener('keydown', function(theEvent) {
 function compButFunc(){
   var selectList = $('[name="ticklerview"]')[0]
   selectList.value = "C"
-  var reportBut = $('input[value*="Report"]')[0]
+  var reportBut = $('.mbttn[value*="Report"]')[0]
   reportBut.click()
-
+  
 }
 
