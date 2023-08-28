c225a2a1ed36da74c1090c4a6c89aaa2a4b8e9fb
sggongshow <87620031+sggongshow@users.noreply.github.com>
2023-08-28 13:01:46 -0700

    Update BillingCodeToText.user.js

--- BillingCodeToText.user.js
+++ BillingCodeToText.user.js
@@ -10,33 +10,9 @@
 // @include     *formwcb.do?*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version 	  22.05.17.2
+// @version 	  23.08.28.0
 // ==/UserScript==
 
+//changelog
+//23.08.28.0 archived project
 
-//wait window load first
-window.addEventListener('load', function() {
-  
-   setTimeout(function(){ main(); }, 250);
-  
-}, false);
-
-
-function main(){
-
-  var codeList = $('a[href*=myFunction][onclick*=PickD]')
-  console.log(codeList)
-
-	for (let i = 0; i < codeList.length; i++) {
-  	
-    var title = codeList[i].title
-    var dxCode = codeList[i].text.trim()
-
-    codeList[i].text = "- " + title + dxCode
-		codeList[i].title = dxCode
-	}
-
-  
-  
-
-}
