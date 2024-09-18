82461496c587a14e117519beeeb809e61e26e6ff
sggongshow <87620031+sggongshow@users.noreply.github.com>
2024-09-18 10:07:27 -0700

    Update CPSBCSearch.user.js

--- CPSBCSearch.user.js
+++ CPSBCSearch.user.js
@@ -6,7 +6,7 @@
 // @include			*www.cpsbc.ca/public/registrant-directory/search-result*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version		  22.05.17.2
+// @version		  24.09.18.1
 // @grant           GM.setValue
 // @grant           GM.getValue
 // @grant						GM.openInTab
@@ -16,13 +16,13 @@
 
 window.addEventListener('load', function () {
 (async () => {
-  
+
 var url = window.location.href
 var docNameFind
 var docNameField
-    
+
 	if (url.indexOf('labDisplay') >0){
-    
+
     docNameFind = $(".FieldData")
   	for (var i = 0; i < docNameFind.length; i++){
         if (docNameFind[i].innerText.indexOf("Requesting Client") >= 0){
@@ -32,7 +32,7 @@ var docNameField
     }
 
     var parentField = docNameField
-    
+
     var input1 = document.createElement('input');
     input1.type = 'button';
     input1.value = 'Check Speciality';
@@ -40,25 +40,25 @@ var docNameField
     input1.setAttribute('style', 'width:100px;font-size:16px;');
     parentField.append(input1);
 
-    
+
     //runDocNameCheck()
 
   }
   else{
     console.log("not oscar")
-    
+
     var lastNameBox = $("#edit-ps-last-name")[0]
     var firstNameBox = $('input[id*=edit-ps-first-name]')[0]
     console.log(GM.getValue('last', 0))
    	var test = GM.getValue('last', 0)
-    
 
-    
+
+
     lastNameBox.value = await GM.getValue('last', 0)
     firstNameBox.value = await GM.getValue('first', 0)
     var counter = await GM.getValue('count', 0)
     var subBut = $('input[id*=edit-ps-submit]')[0]
-    
+
     console.log(counter)
    	if (counter >0){
       console.log('run once test')
@@ -66,9 +66,11 @@ var docNameField
       GM.setValue("count", 0)
     }
 
-   
+    GM.setValue("last", null)
+    GM.setValue("first", null)
+
   }
-    
+
   function runDocNameCheck()
   {
     var docName = ''
@@ -89,10 +91,11 @@ var docNameField
     GM.setValue("last", docNameLast)
     GM.setValue("first", docNameFirst)
     GM.setValue("count", 100)
-    var windowCPSBC = window.open("https://www.cpsbc.ca/public/registrant-directory/search-result")
+    //var windowCPSBC = window.open("https://www.cpsbc.ca/public/registrant-directory/search-result")
+    var windowCPSBC = window.open("https://www.cpsbc.ca/public/registrant-directory")
     //GM.openInWindow("https://www.cpsbc.ca/public/registrant-directory/search-result")
-	} 
-  
+	}
+
 
 
 })();
